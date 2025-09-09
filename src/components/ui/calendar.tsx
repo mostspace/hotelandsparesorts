855d5aebd1873f3649f3cb2980a9606573225ae5
mostspace1027 <mostspace1027@gmail.com>
2025-09-09 17:39:44 +0100

    Date picker change

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -28,6 +28,7 @@ function Calendar({
 
   return (
     <DayPicker
+      autoFocus={false}
       showOutsideDays={showOutsideDays}
       className={cn(
         "bg-background group/calendar p-3 [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent",
