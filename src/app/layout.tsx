59ecf3954986df5b3e56f4f05562ad4bef47733a
mostspace <mostspace1027@gmail.com>
2025-10-14 14:15:00 -0700

    Add GTM container to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,13 @@ export default function RootLayout({
     // }}
   >
     <head>
+    {/* Google Tag Manager */}
+    <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M5JFHRM4');`}} />
+    {/* End Google Tag Manager */}
     <meta name="viewport" content="width=device-width, initial-scale=1" />
   </head>
     
@@ -45,6 +52,12 @@ export default function RootLayout({
         // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         className="font-sans antialiased bg-[#f7f7f7] "
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5JFHRM4"
+          height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <div className="app-wrapper">
           <Header />
 
